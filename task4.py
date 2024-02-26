e53b7d1c10cd3349f74620391836d1f0e1bcfce3
ArinaPanfilova <58852877+ArinaPanfilova@users.noreply.github.com>
2024-02-26 17:10:51 +0300

    Add files via upload
    файлы по 4 заданию

---
+++ task4.py
@@ -0,0 +1,25 @@
+def create_pass(s):
+    #  ''' Функция генерации пароля, где <s> - список данных по одному кораблю
+    n = ""
+    n += s[1][-3:]
+    n += s[0].split("-")[0][1:3][::-1]
+    n += s[1][:3][::-1]
+    return n.upper()
+
+
+f = open("space.txt").read()
+f = f.split("\n")
+header = f.pop(0)
+data = [i.split("*") for i in f]
+
+#  ''' сверху подготовка данных для работы '''
+
+#  ''' снизу перезапись данных вместе с паролем '''
+
+ans = open("space_uniq_password.csv", "w")
+ans.write(";".join(header.split("*")) + ";" + "password")
+for i in data:
+    pas = create_pass(i)
+    ans.write("\n")
+    ans.write(";".join(i) + ";" + f'{pas}')
+ans.close()
